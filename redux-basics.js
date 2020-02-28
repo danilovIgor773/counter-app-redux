79638b47d2ea98a3ca69d8c626809ebc66603bf0
Danilov Igor <danilovigor773@gmail.com>
2020-02-28 14:36:02 +0500

    Added action dispatching an dsome logic for root reducer.

--- redux-basics.js
+++ redux-basics.js
@@ -7,13 +7,31 @@ const initialState = {
 }
 
 const rootReducer = (state = initialState, action) => {
-    return state;
+    
+    switch(action.type){
+        case 'INC_COUNTER':
+            return{
+                ...state,
+                counter: state.counter + 1
+            };
+        case 'ADD_COUNTER':
+            return{
+                ...state,
+                counter: state.counter + action.value
+            };
+        default: 
+            return state;
+    }
 }
 
 const store = createStore(rootReducer);
 
 console.log("Store is: ", store.getState());
 
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 7});
+
+console.log("Store is: ", store.getState());
 
 //Store
 
