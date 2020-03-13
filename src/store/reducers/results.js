94e645c1b3cb71b88ad81acabcdcb531d74d7a9e
Danilov Igor <danilovigor773@gmail.com>
2020-03-13 16:42:32 +0500

    Made some minor changes to result-reducer to make it leaner.

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -6,16 +6,19 @@ const initialState = {
     results: []
 }
 
+//here (it's not necessary) we can write function (wrapper) to make switch statement more leaner
+
+const deleteResults = (state, action) => {
+    const updatedArr = state.results.filter(res => res.id !== action.resId);
+    return updateObject(state, {results: updatedArr});
+}
+
 const resultReducer = (state = initialState, action ) => {
     
     switch(action.type){
-        case actions.STORE_RESULTS:
-            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
-        case actions.DELETE_RESULTS:
-            const updatedArr = state.results.filter(res => res.id !== action.resId);
-            return updateObject(state, {results: updatedArr});
-        default: 
-            return state;
+        case actions.STORE_RESULTS: return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
+        case actions.DELETE_RESULTS: return deleteResults(state, action);
+        default: return state;
     }
 }
 
